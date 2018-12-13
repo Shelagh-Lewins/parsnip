4b41b2926241c0d1979c51b000724e1893d4d24b
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-12-13 15:10:48 +0000

    initial commit

---
+++ assets/precache-manifest.fded0380c99cc68485660a6c2f39a89e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce7a10c61e1141b879e4",
+    "url": "/static/js/main.ce7a10c6.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "ce7a10c61e1141b879e4",
+    "url": "/static/css/main.42a305af.chunk.css"
+  },
+  {
+    "revision": "34f75a718f9c3a743979e1b90bef6ffc",
+    "url": "/index.html"
+  }
+];
