fec83c1bf2f2facb4c4f75fdc75de9076988941e
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-12-25 15:22:19 +0000

    made default selected list and items match

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,6 @@ import { LIST_IS_PUBLIC_TEXTS } from '../constants';
 
 class Header extends Component {
 	render() {
-		// console.log('props ', this.props);
 		const listOptions = Object.keys(this.props.lists).map(is_public => {
 			const listsByIsPublic = this.props.lists[is_public];
 			const options = listsByIsPublic.map(list => {
@@ -19,8 +18,6 @@ class Header extends Component {
 
 			return options;
 		});
-
-		// console.log('currentListId ', this.props.currentListId);
 
 		let listTitle;
 		let listIsPublic;
@@ -37,21 +34,17 @@ class Header extends Component {
 			}
 		});
 
-		// hacky code to set default value matching the addition of 'Public' or 'Private' to the option text. This is NOT how anything should be done in the real world.
-		// TODO recheck default selection after refresh and make sure correct items are loaded.
-		const index = LIST_IS_PUBLIC_VALUES.indexOf(listIsPublic === 'true');
-
-		console.log('list name ', listTitle);
-
-		const selected = `${listTitle} (${LIST_IS_PUBLIC_TEXTS[index]})`;
-		console.log('selected ', selected);
-
+		// don't render select until default selection is available
 		return (
-			<div className="list-select">
-				List:
-				<select onChange={this.props.onCurrentListChange} className="list=menu" defaultValue={{ 'value': selected }}>
-					{listOptions}
-				</select>
+			<div className="header">
+				{this.props.currentListId && (
+					<div className="list-select">
+						List:
+						<select onChange={this.props.onCurrentListChange} className="list=menu" value={this.props.currentListId}>
+							{listOptions}
+						</select>
+					</div>
+				)}
 			</div>
 		);
 	};
